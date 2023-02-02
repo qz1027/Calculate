82dc3e16442fb58b425035cee5880365e1632fec
qz1027 <3315057006@qq.com>
2023-02-02 21:21:00 +0800

    嘿嘿

--- Calculate/Calculate.cpp
+++ Calculate/Calculate.cpp
@@ -10,15 +10,9 @@ double Calculate::low(double x, double y)
     return x - y;
 }
 
-double Calculate::chufa(double x, double y)
-{
-    return x / y;
-}
-
 void Calculate::menu()
 {
     cout << "\t1、加法功能" << endl;
     cout << "\t2、减法功能" << endl;
-    cout << "\t3、除法功能" << endl;
     cout << "\t0、退出" << endl;
 }
